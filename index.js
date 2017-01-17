bdba61fea2eda1b7e2dfeb6a2e934f5c927d620d
Scott Rich <scott.rich.nc@gmail.com>
2017-01-16 22:31:38 -0500

    first pass of EM, SS, T workouts

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-// import _ from "lodash";
+import _ from "lodash";
 import yargs from "yargs";
 import fsp from "fs-promise";
+import path from "path";
 
 export default class ZwiftWorkouts {
     constructor(args) {
         this.inputFile = args.file;
-        this.outputFile = args.o;
+        this.outputDir = args.o;
+    }
+
+    loadWorkouts() {
         Promise.all(
             [
                 fsp.stat(this.inputFile)
@@ -13,17 +17,81 @@ export default class ZwiftWorkouts {
                         if (!stat.isFile) throw Error("Invalid input file");
                         return fsp.realpath(this.inputFile);
                     })
-                    .then((realPath) => { return `Reading workouts from ${realPath}` }),
-                fsp.stat(this.outputFile)
+                    .then(realPath => { return realPath }),
+                fsp.stat(this.outputDir)
                     .then((stat) => {
-                        if (!stat.isDirectory) throw Error("Invalid output directory")
-                        return fsp.realpath(this.outputFile);
+                        if (!stat.isDirectory) throw Error("Invalid output directory");
+                        return fsp.realpath(this.outputDir);
                     })
-                    .then((realPath) => { return `Writing workouts to ${realPath}` }),
+                    .then(realPath => { return realPath })
             ]
         )
-            .then(values => { console.log(values) })
+            .then(paths => {
+                this.inputFile = paths[0];
+                this.outputDir = paths[1];
+                console.log(`Loading workouts from ${this.inputFile}`);
+                return fsp.readJSON(this.inputFile)
+                    .then(workouts => {
+                        console.log('Successfully loaded workouts.');
+                        return workouts;
+                    })
+                    .catch(e => {
+                        console.log(`Error loading workouts: ${e}`)
+                    });
+            })
+            .then(workouts => {
+                console.log(`Writing workouts to ${this.outputDir}`);
+                // console.log(`${JSON.stringify(workouts)}`);
+                return fsp.ensureDir(this.outputDir)
+                    .then(fsp.ensureDir(path.join(this.outputDir, workouts.name)))
+                    .then(() => {
+                        const rootDir = path.join(this.outputDir, workouts.name);
+                        _.each(_.omit(workouts, "name"), (days, week) => {
+                            const weekDir = path.join(rootDir, week);
+                            fsp.ensureDir(weekDir)
+                                .then(() => {
+                                    _.each(days, (segments, day) => {
+                                        const workoutFileName = path.join(weekDir, `${day}.zwo`);
+                                        const workoutName = `${workouts.name} - ${week} - ${day}`
+                                        const segmentsText = segments.map(this.segmentText).join("\n");
+                                        const workoutTemplate = `<workout_file>
+    <author>s.rich</author>
+    <name>${workoutName}</name>
+    <description></description>
+    <sportType>bike</sportType>
+    <tags>
+    </tags>
+    <workout>
+       <Warmup Duration="360" PowerLow="0.35" PowerHigh="0.60"/>
+${segmentsText}
+       <Cooldown Duration="360" PowerLow="0.60" PowerHigh="0.35"/>
+    </workout>
+</workout_file>
+`;
+                                        fsp.writeFileSync(workoutFileName, workoutTemplate);
+                                    })
+                                });
+                        });
+                    });
+            })
             .catch((e) => { console.log(e) });
+    }
+
+    segmentText(s) {
+        const type = s[0];
+        switch (type) {
+            case "EM":
+                return `       <SteadyState Duration="${s[1] * 60}" Power="0.60"/>`
+                break;
+            case "T":
+                return `       <SteadyState Duration="${s[1] * 60}" Power="0.82"/>`
+                break;
+            case "SS":
+                return `       <IntervalsT Repeat="${s[1]}" OnDuration="${s[2] * 60}" OffDuration="${s[3] * 60}" OnPower="0.90" OffPower="0.60"/>`
+                break;
+            default:
+                break;
+        }
     }
 }
 
@@ -33,6 +101,9 @@ const argv = yargs
     .demandOption(["file"])
     .argv;
 
-new ZwiftWorkouts(argv);
+const z = new ZwiftWorkouts(argv);
+
+z.loadWorkouts();
+
 
 
